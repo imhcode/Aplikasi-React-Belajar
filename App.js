7469288c09279547957b7a02a6c72d96bfcc92a8
Fiqi Arifianto <fiqi.afui@gmail.com>
2022-06-01 19:40:38 +0700

    basic styling

--- App.js
+++ App.js
@@ -1,24 +1,85 @@
 import React, { Component } from 'react';
-import {Text, View, Image, TextInput} from 'react-native';
+import {Text, View, Image, TextInput, StyleSheet} from 'react-native';
 import { getAutomaticTypeDirectiveNames } from 'typescript';
+import flower from './image.jpg';
 
 
 const App = () => {
+  return <StylingReactNativeComponent/>
+
+};
+
+
+const StylingReactNativeComponent = () =>{
   return (
     <View>
-      <View style={{width:80,height:80, backgroundColor:'#fe2'}} />
-      <Text>Fiqi</Text>
-      <Home/>
+      <Text style={styles.text} >Styling Componnet</Text>
+      <View
+        style={{
+          width:100,
+          height:100,
+          backgroundColor:'#ff3ddd',
+          borderWidth:2,
+          marginTop:10,
+          marginLeft:10
 
-      <Text>Arifianto</Text>
-      <Text>Sudjono</Text>
-      <Photo/>
-      <TextInput style={{borderWidth:1}}></TextInput>
-      <BoxGreen/>
-      <Profile/>
+        }}
+      >
+      </View>
+      <View style={{padding:12, backgroundColor:'#f2f2f2', width:212, borderRadius:8}}>
+        <Image source={flower} style={{width:188, height:107, borderRadius:8}} ></Image>
+        <Text style={{fontSize:14, fontWeight:'bold', marginTop:16}} >Flower Indah</Text>
+        <Text style={{fontSize:12, fontWeight:'bold',color:'#ffa000', marginTop:12}} >Rp. 50.000,-</Text>
+        <Text style={{fontSize:12, fontWeight:'300', marginTop:12}} >Jakarta Barat</Text>
+        <View style={{
+          backgroundColor:'#6fcf97',
+          paddingVertical:6,
+          borderRadius:25,
+          marginTop:20
+        }}>
+          <Text
+            style={{
+              textAlign:'center',
+              color:'#fff',
+              fontWeight:'600',
+              fontSize:14,
+
+            }}
+          >Beli</Text>
+        </View>
+      </View>
+
     </View>
-    );
-};
+  )
+}
+
+const styles = StyleSheet.create({
+  text:{
+    fontSize:18,
+    color:'#ff3ddd',
+    fontWeight:'bold',
+    marginTop:10,
+    marginLeft:10,
+  }
+});
+
+const SampleComponent = () =>{
+return (
+  
+  <View>
+  <View style={{width:80,height:80, backgroundColor:'#fe2'}} />
+  <Text>Fiqi</Text>
+  <Home/>
+
+  <Text>Arifianto</Text>
+  <Text>Sudjono</Text>
+  <Photo/>
+  <TextInput style={{borderWidth:1}}></TextInput>
+  <BoxGreen/>
+  <Profile/>
+</View>
+)
+}
 
 const Home = () =>{
   return (
